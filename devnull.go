551730057c588fcbf013eaa97d51f4322e212536
Alexander Rødseth <rodseth@gmail.com>
2013-04-25 13:08:06 +0200

    Added two functions

--- devnull.go
+++ devnull.go
@@ -1,12 +1,23 @@
 package genericsite
 
-// Dismisses various attempts at accessing this site
+// Dismisses various attempts at accessing this site. Honeypot?
 
 import (
+	"fmt"
+
+	"github.com/xyproto/instapage"
 	"github.com/xyproto/web"
 )
 
-// Honeypot?
+func Hello() string {
+	msg := "Hi"
+	return instapage.Message("Hello", msg)
+}
+
+func ParamExample(ctx *web.Context) string {
+	return fmt.Sprintf("%v\n", ctx.Params)
+}
+
 func ServeForFun() {
 	// These appeared in the log
 	bogus := []string{"/signup", "/wp-login.php", "/join.php", "/register.php", "/profile.php", "/user/register/", "/tools/quicklogin.one", "/sign_up.html", "/profile.php", "/ucp.php", "/account/register.php", "/join_form.php", "/tiki-register.php", "/YaBB.cgi/", "/YaBB.pl/", "/member/register", "/signup.php", "/blogs/load/recent", "/member/join.php"}
